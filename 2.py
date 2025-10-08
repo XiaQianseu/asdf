76622ff544af2dce6b3c7397f1dcd8d56efbca1e
XiaQianseu <yn_xiaqian@163.com>
2025-10-08 10:41:50 +0800

    2.py

---
+++ 2.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("我的第一个 Streamlit 网页")
+st.write("这是一个网页，而不是桌面窗口。")
+
+x = st.number_input("输入一个数", 0, 100, 10)
+y = st.number_input("再输入一个数", 0, 100, 20)
+
+st.write("两数之和是：", x + y)
